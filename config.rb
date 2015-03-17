30a23b394d713f33e3d69f262bf69a48418bf910
DevinClark <dclarkdesign@gmail.com>
2015-03-16 22:19:10 -0500

    Add donors page techlahoma/planning#13
    TODO:
    * responsive tweaks
    * real data

--- config.rb
+++ config.rb
@@ -24,6 +24,8 @@
 #   page "/admin/*"
 # end
 
+page "/donors.html", :layout => :slim_layout
+
 # Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
@@ -70,7 +72,6 @@ configure :build do
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
 end
-
 
 activate :deploy do |deploy|
   deploy.method = :git
