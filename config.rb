85922335a9cd48e9125ad414d65d7026ecdfe7b1
Ryan Hoegg <ryan.hoegg@gmail.com>
2015-07-09 23:51:49 -0500

    Fix typo in deployment config

--- config.rb
+++ config.rb
@@ -80,5 +80,5 @@ activate :deploy do |deploy|
   # deploy.branch   = "custom-branch" # default: gh-pages
   # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
   # deploy.commit_message = "custom-message"      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
-  deplot.commit_message = "Deployed with Middleman [skip ci]"
+  deploy.commit_message = "Deployed with Middleman [skip ci]"
 end
